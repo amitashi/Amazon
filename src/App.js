ee1e677d934647ad584379c63ca56b1da7526b3b
amitashi <amitkumar280198@gmail.com>
2023-04-27 00:40:56 +0530

    initialversion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Header from "./components/header";
+import { Img } from "@chakra-ui/react";
+import testimg from "./helpers/dell_days_3000x1200._CB591033943_.svg";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Img src={testimg} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,159 @@
+import { Box, Flex, Img, Input, Text } from "@chakra-ui/react";
+import React from "react";
+import amazonicon from "../helpers/logo-amazon.svg";
+import serachicon from "../helpers/magnifying-glass-solid.svg";
+import carticon from "../helpers/cart-shopping-solid.svg";
+import homeicon from "../helpers/bars-solid.svg";
+const Header = () => {
+  return (
+    <>
+      {/* // top logo bg="#232F3E"*/}
+      <Flex flexGrow={1} p={1} alignItems={"center"} bg="#131921">
+        <Flex
+          mx={1}
+          flexGrow={{ base: 1, sm: 0 }}
+          alignItems={"center"}
+          justifyContent={"center"}
+          p={1}
+          m={0}
+        >
+          <Img
+            fill={"whiteAlpha.100"}
+            src={amazonicon}
+            alt="icon"
+            width={"5rem"}
+            height={"3rem"}
+            objectFit={"contain"}
+            cursor={"pointer"}
+          />
+        </Flex>
+        {/* {search} */}
+        <Flex
+          display={{ base: "none", sm: "flex" }}
+          alignItems={"center"}
+          bg={"yellow.400"}
+          _hover={{
+            bg: "yellow.500",
+          }}
+          mx={2}
+          borderRadius={"0.4rem"}
+          flexGrow={1}
+          cursor={"pointer"}
+          flexShrink={1}
+        >
+          <Input
+            bg={"white"}
+            type="text"
+            display="flex"
+            flexGrow={1}
+            h={"100%"}
+            p={2}
+            borderRightRadius={0}
+            flexShrink={1}
+            _focusVisible={{ outline: "none" }}
+          />
+          <Img src={serachicon} alt="icon" h={5} w={12} />
+        </Flex>
+        {/* right options */}
+        <Flex
+          alignItems={"center"}
+          color={"white"}
+          mx={2}
+          fontSize={"xs"}
+          justifyContent={"space-between"}
+          whiteSpace={"nowrap"}
+          px={4}
+        >
+          <Box mx={3} cursor={"pointer"} _hover={{ textDecor: "underline" }}>
+            <p>Hello, Amit</p>
+            <Text fontWeight={"bold"}>Account & Lists</Text>
+          </Box>
+          <Box mx={3} cursor={"pointer"} _hover={{ textDecor: "underline" }}>
+            <p>Returns</p>
+            <Text fontWeight={"bold"}>& Orders</Text>
+          </Box>
+          <Box
+            position="relative"
+            display={"flex"}
+            flexDir={"row"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            mx={3}
+            cursor={"pointer"}
+            _hover={{ textDecor: "underline" }}
+            w={10}
+          >
+            <Text
+              position={"absolute"}
+              right={{ base: -1, sm: 3 }}
+              top={{ base: -2, sm: 0 }}
+              bg={"yellow.400"}
+              w={4}
+              h={4}
+              textAlign={"center"}
+              fontWeight={"bold"}
+              borderRadius={"full"}
+              color={"black"}
+              size={"lg"}
+            >
+              0
+            </Text>
+            <Img src={carticon} h={6} w={6} />
+            <Text
+              display={{ base: "none", sm: "inline" }}
+              fontWeight={"bold"}
+              mt={4}
+            >
+              Basket
+            </Text>
+          </Box>
+        </Flex>
+      </Flex>
+      {/* bottom nav */}
+      <Flex alignItems={"center"} pl={2} p={1} bg="#232F3E" color={"white"}>
+        <Img src={homeicon} h={4} w={4} display={"inline"} mx={1} ml={2} />
+        <Text mx={2}>All</Text>
+        <Text cursor={"pointer"} _hover={{ textDecor: "underline" }}>
+          Prime Video
+        </Text>
+        <Text mx={2} cursor={"pointer"} _hover={{ textDecor: "underline" }}>
+          Amazon Business
+        </Text>
+        <Text
+          mx={2}
+          display={{ base: "none", lg: "inline" }}
+          cursor={"pointer"}
+          _hover={{ textDecor: "underline" }}
+        >
+          Today's Deal
+        </Text>
+        <Text
+          mx={2}
+          display={{ base: "none", lg: "inline" }}
+          cursor={"pointer"}
+          _hover={{ textDecor: "underline" }}
+        >
+          Electronics
+        </Text>
+        <Text
+          mx={2}
+          display={{ base: "none", lg: "inline" }}
+          cursor={"pointer"}
+          _hover={{ textDecor: "underline" }}
+        >
+          Foods & Grocery
+        </Text>
+        <Text
+          mx={2}
+          display={{ base: "none", lg: "inline" }}
+          cursor={"pointer"}
+          _hover={{ textDecor: "underline" }}
+        >
+          Buy Again
+        </Text>
+      </Flex>
+    </>
+  );
+};
+
+export default Header;
